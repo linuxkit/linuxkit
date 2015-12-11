91aff8050aad9a8603325388cc553925abf2d037
Justin Cormack <justin.cormack@unikernel.com>
2015-12-11 11:39:50 +0000

    add virtio rng device in xhyve boot
    Signed-off-by: Justin Cormack <justin.cormack@unikernel.com>

--- xhyve/xhyverun.sh
+++ xhyve/xhyverun.sh
@@ -16,9 +16,10 @@ fi
 #IMG_CD="-s 3,ahci-cd,/somepath/somefile.iso"
 #IMG_HDD="-s 4,virtio-blk,/somepath/somefile.img"
 PCI_DEV="-s 0:0,hostbridge -s 31,lpc"
+RND="-s 5,virtio-rnd"
 LPC_DEV="-l com1,stdio"
 ACPI="-A"
 CLOCK="-u"
 #UUID="-U deadbeef-dead-dead-dead-deaddeafbeef"
 
-build/xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_CD $IMG_HDD $UUID $CLOCK -f kexec,$KERNEL,$INITRD,"$CMDLINE"
+build/xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_CD $IMG_HDD $RND $UUID $CLOCK -f kexec,$KERNEL,$INITRD,"$CMDLINE"
