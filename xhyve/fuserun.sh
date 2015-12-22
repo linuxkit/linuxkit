9c49e0b5b5e930be95d7d21af98395f283774b4b
David Sheets <sheets@alum.mit.edu>
2015-12-22 18:00:05 +0000

    Add 9pudfuse and fuserun.sh for testing remote FUSE file systems

---
+++ xhyve/fuserun.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+KERNEL="../kernel/vmlinuz64"
+INITRD="../alpine/initrd.img"
+CMDLINE="earlyprintk=serial console=ttyS0 virtio_pci.force_legacy=1"
+
+MEM="-m 1G"
+#SMP="-c 2"
+NET=""
+if (( $EUID != 0 )); then
+    printf "Warning: not running as root will have no networking!\n\n"
+    sleep 1
+else
+NET="-s 2:0,virtio-net"
+fi
+#IMG_CD="-s 3,ahci-cd,/somepath/somefile.iso"
+#IMG_HDD="-s 4,virtio-blk,/somepath/somefile.img"
+PCI_DEV="-s 0:0,hostbridge -s 31,lpc"
+RND="-s 5,virtio-rnd"
+FS="-s 6,virtio-9p,path=9pudfuse.sock,tag=fuse"
+LPC_DEV="-l com1,stdio"
+ACPI="-A"
+CLOCK="-u"
+#UUID="-U deadbeef-dead-dead-dead-deaddeafbeef"
+
+build/xhyve $ACPI $MEM $SMP $PCI_DEV $LPC_DEV $NET $IMG_CD $IMG_HDD $RND $FS $UUID $CLOCK -f kexec,$KERNEL,$INITRD,"$CMDLINE"
