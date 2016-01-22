90dd1fb27351ca7a3bfe006f92be26e6ef0a30d0
Justin Cormack <justin.cormack@docker.com>
2016-01-22 18:36:34 +0000

    improve script
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- scripts/moby.sh
+++ scripts/moby.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-cp ../alpine/initrd.img /Applications/Docker.app/Contents/Resources/moby/initrd.img
-cp ../alpine/kernel/vmlinuz64 /Applications/Docker.app/Contents/Resources/moby/vmlinuz64
+cp alpine/initrd.img /Applications/Docker.app/Contents/Resources/moby/initrd.img
+cp alpine/kernel/vmlinuz64 /Applications/Docker.app/Contents/Resources/moby/vmlinuz64
 
 killall com.docker.driver.amd64-linux
+
+sleep 2
 
 time docker ps
