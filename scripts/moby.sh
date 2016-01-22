e0acb831a8545ae67ccd112e544027a690fdc377
Justin Cormack <justin.cormack@docker.com>
2016-01-22 17:35:56 +0000

    script to restart docker4mac
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

---
+++ scripts/moby.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+cp ../alpine/initrd.img /Applications/Docker.app/Contents/Resources/moby/initrd.img
+cp ../alpine/kernel/vmlinuz64 /Applications/Docker.app/Contents/Resources/moby/vmlinuz64
+
+killall com.docker.driver.amd64-linux
+
+time docker ps
