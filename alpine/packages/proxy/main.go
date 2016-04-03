00ff8897a77046243133391f8cb21dbc3e8e0477
David Scott <dave@recoil.org>
2016-04-03 14:57:35 +0100

    proxy: add `exposePort` and `unexposePort` stubs
    This patch removes `proxyForever` and adds `sendError` and `sendOK` for
    signalling to the parent process. The main function now sequences these
    functions and calls stub functions `exposePort` and `unexposePort` which
    will be hooked up in a later patch.
    
    Signed-off-by: David Scott <dave.scott@docker.com>

--- alpine/packages/proxy/main.go
+++ alpine/packages/proxy/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net"
 	"os"
 	"pkg/proxy"
 )
@@ -9,10 +10,27 @@ import (
 func main() {
 	host, container := parseHostContainerAddrs()
 
-	err := proxyForever(proxy.NewProxy(host, container))
-
+	err := exposePort(host)
 	if err != nil {
-		os.Exit(0)
+		sendError(err)
 	}
-	os.Exit(1)
+	p, err := proxy.NewProxy(host, container)
+	if err != nil {
+		unexposePort(host)
+		sendError(err)
+	}
+	go handleStopSignals(p)
+	sendOK()
+	p.Run()
+	unexposePort(host)
+	os.Exit(0)
+}
+
+func exposePort(host net.Addr) error {
+	log.Printf("exposePort %#v\n", host)
+	return nil
+}
+
+func unexposePort(host net.Addr) {
+	log.Printf("unexposePort %#v\n", host)
 }
