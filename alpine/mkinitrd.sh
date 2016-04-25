217cfe354e80a9e3066b01ac1e34250339d41f62
Justin Cormack <justin.cormack@docker.com>
2016-04-25 12:00:48 +0100

    tidy up default device creation
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- alpine/mkinitrd.sh
+++ alpine/mkinitrd.sh
@@ -14,6 +14,9 @@ mkdir -m 1777 /tmp/tmp
 
 cd /tmp/dev
 
+# start from clean slate
+rm -rf *
+
 mknod -m 666 null c 1 3
 mknod -m 666 full c 1 7
 mknod -m 666 ptmx c 5 2
@@ -21,8 +24,10 @@ mknod -m 644 random c 1 8
 mknod -m 644 urandom c 1 9
 mknod -m 666 zero c 1 5
 mknod -m 666 tty c 5 0
+mknod -m 600 console c 5 1
 
 mknod -m 600 ttyS0 c 4 64
+mknod -m 600 fuse c 10 229
 
 # we are using sata emulation at present
 mknod -m 600 sda b 8 0
@@ -40,7 +45,26 @@ mknod -m 600 sdb4 b 8 20
 mknod -m 600 sdb5 b 8 21
 mknod -m 600 sdb6 b 8 22
 
-mkdir pty
+# mount points in /dev
+mkdir pts mqueue shm
+
+# symlinks
+ln -s /proc/kcore core
+ln -s /proc/self/fd fd
+ln -s /proc/self/fd/0 stdin
+ln -s /proc/self/fd/1 stdout
+ln -s /proc/self/fd/2 stderr
+
+# loop devs
+mknod -m 600 loop-control c 10 237
+mknod -m 600 loop0 c 7 0
+mknod -m 600 loop1 c 7 1
+mknod -m 600 loop2 c 7 2
+mknod -m 600 loop3 c 7 3
+mknod -m 600 loop4 c 7 4
+mknod -m 600 loop5 c 7 5
+mknod -m 600 loop6 c 7 6
+mknod -m 600 loop7 c 7 7
 
 # these three files are bind mounted in by docker so they are not what we want
 
