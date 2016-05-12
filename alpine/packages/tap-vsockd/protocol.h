616b1c1f6678fa7d61edf48df52e50d6592f586b
David Scott <dave@recoil.org>
2016-05-12 16:07:51 +0100

    tap-vsockd: remove dead code and ASL references
    Signed-off-by: David Scott <dave.scott@docker.com>

--- alpine/packages/tap-vsockd/protocol.h
+++ alpine/packages/tap-vsockd/protocol.h
@@ -25,14 +25,8 @@ extern char *print_init_message(struct init_message *m);
 /* Client -> Server command */
 enum command {
   ethernet = 1,
-  uninstall = 2,
-  install_symlinks = 3,
-  uninstall_symlinks = 4,
-  uninstall_sockets = 5, // to uninstall all sockets but com.docker.vmnetd.socket
-  bind_ipv4 = 6,
 };
 
-extern int read_command(int fd, enum command *c);
 extern int write_command(int fd, enum command *c);
 
 /* Client -> Server command arguments */
@@ -40,7 +34,6 @@ struct ethernet_args {
   char uuid_string[36];
 };
 
-extern int read_ethernet_args(int fd, struct ethernet_args *args);
 extern int write_ethernet_args(int fd, struct ethernet_args *args);
 
 /* Server -> Client: details of a vif */
@@ -59,18 +52,5 @@ extern char expected_hello_old[5];
 
 extern int really_read(int fd, uint8_t *buffer, size_t total);
 extern int really_write(int fd, uint8_t *buffer, size_t total);
-extern int really_writev(int fd, struct iovec *iov, int iovcnt);
-
-// Client -> Server: requested IPv4 address and port
-struct bind_ipv4 {
-  uint32_t ipv4;
-  short _padding;
-  uint16_t port;
-  uint8_t stream; /* 0 = stream; 1 = dgram */
-  uint8_t _padding2[3];
-};
-
-extern int read_bind_ipv4(int fd, struct bind_ipv4 *vif);
-extern int write_bind_ipv4(int fd, struct bind_ipv4 *vif);
 
 #endif /* _VMNET_PROTOCOL_H_ */
