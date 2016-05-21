6fea25a0f0799577b8dff7b798f320cafe92b227
Nathan LeClaire <nathan.leclaire@gmail.com>
2016-05-20 19:29:24 -0700

    Correct script error
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- alpine/aws/joiner-user-data.sh
+++ alpine/aws/joiner-user-data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 logcmd() {
-    "$@" | awk -v timestamp="$(date) " '$0=timestamp$0' >>/var/log/docker-swarm.log 2&>1
+    "$@" | awk -v timestamp="$(date) " '$0=timestamp$0' >>/var/log/docker-swarm.log 2>&1
 }
 
 logcmd docker swarm join {{MANAGER_IP}}:4500
