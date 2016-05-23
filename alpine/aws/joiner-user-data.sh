e5d3c4c4843e7593bb27e14376a590e979996bd8
Nathan LeClaire <nathan.leclaire@gmail.com>
2016-05-23 11:13:01 -0700

    Fix issues with example manager and joiner user data
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- alpine/aws/joiner-user-data.sh
+++ alpine/aws/joiner-user-data.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 logcmd() {
-    "$@" | awk -v timestamp="$(date) " '$0=timestamp$0' >>/var/log/docker-swarm.log 2>&1
+    "$@" 2>&1 | awk -v timestamp="$(date) " '$0=timestamp$0' >>/var/log/docker-swarm.log
 }
 
-logcmd docker swarm join {{MANAGER_IP}}:4500
-logcmd docker swarm info
+for i in $(seq 1 20); do
+    if [ -S /var/run/docker.sock ]; then
+        logcmd docker swarm join {{MANAGER_IP}}:4500
+        logcmd docker swarm info
+        exit 0
+    fi
+    sleep 1
+done
+
+exit 1
--- alpine/aws/manager-user-data.sh
+++ alpine/aws/manager-user-data.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-# TODO: This should be handled by openrc maybe? (or by docker service)
+METADATA=http://169.254.169.254/latest/meta-data
+
+# TODO: This dial retry loop should be handled by openrc maybe? (or by docker
+# service)
 for i in $(seq 1 20); do
     if [ -S /var/run/docker.sock ]; then
-        docker swarm create >>/var/log/docker-swarm.log 2>&1
+        docker swarm create \
+            --listen-addr $(wget -qO- ${METADATA}/local-ipv4 | sed 's/http:\/\///'):4500 \
+            >>/var/log/docker-swarm.log 2>&1
         exit 0
     fi
     sleep 1
