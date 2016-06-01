04d8d34042b4c00fe299dc110e88830d08a791ca
Nathan LeClaire <nathan.leclaire@gmail.com>
2016-06-01 15:17:27 -0700

    Add a few aesthetic changes to common.sh script
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- alpine/aws/common.sh
+++ alpine/aws/common.sh
@@ -2,31 +2,31 @@
 
 set -e
 
-TAG_KEY=moby-image
-INSTANCE_ENDPOINT=http://169.254.169.254/latest
-INSTANCE_METADATA_API_ENDPOINT=${INSTANCE_ENDPOINT}/meta-data/
-IMAGE_NAME=${IMAGE_NAME:-"Moby Linux"}
-IMAGE_DESCRIPTION=${IMAGE_DESCRIPTION:-"The best OS for running Docker"}
+TAG_KEY="tp3-dev"
+INSTANCE_ENDPOINT="http://169.254.169.254/latest"
+INSTANCE_METADATA_API_ENDPOINT="${INSTANCE_ENDPOINT}/meta-data/"
+IMAGE_NAME=${IMAGE_NAME:-"Moby Linux ${TAG_KEY}"}
+IMAGE_DESCRIPTION=${IMAGE_DESCRIPTION:-"The best OS for running Docker, version ${TAG_KEY}"}
 
-function arrowecho () {
+arrowecho() {
     echo " --->" "$@"
 }
 
-function current_instance_region () {
-    curl -s ${INSTANCE_ENDPOINT}/dynamic/instance-identity/document | jq .region -r
+current_instance_region() {
+    curl -s "${INSTANCE_ENDPOINT}/dynamic/instance-identity/document" | jq .region -r
 }
 
-function current_instance_az () {
-    curl -s ${INSTANCE_METADATA_API_ENDPOINT}/placement/availability-zone
+current_instance_az() {
+    curl -s "${INSTANCE_METADATA_API_ENDPOINT}/placement/availability-zone"
 }
 
-function current_instance_id () {
-    curl -s ${INSTANCE_METADATA_API_ENDPOINT}/instance-id
+current_instance_id() {
+    curl -s "${INSTANCE_METADATA_API_ENDPOINT}/instance-id"
 }
 
 # We tag resources created as part of the build to ensure that they can be
 # cleaned up later.
-function tag () {
+tag() {
     arrowecho "Tagging $1"
-    aws ec2 create-tags --resources "$1" --tags Key=${TAG_KEY},Value= >/dev/null
+    aws ec2 create-tags --resources "$1" --tags "Key=${TAG_KEY},Value=" >/dev/null
 }
