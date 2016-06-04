a46a4289806526bcda39304ab0c8eeb49b1d390d
Nathan LeClaire <nathan.leclaire@gmail.com>
2016-06-03 18:51:04 -0700

    Change 'swarm create' to 'swarm init'
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- alpine/aws/manager-user-data.sh
+++ alpine/aws/manager-user-data.sh
@@ -6,7 +6,7 @@ METADATA=http://169.254.169.254/latest/meta-data
 # service)
 for i in $(seq 1 20); do
     if [ -S /var/run/docker.sock ]; then
-        docker swarm create \
+        docker swarm init \
             --listen-addr $(wget -qO- ${METADATA}/local-ipv4 | sed 's/http:\/\///'):4500 \
             >>/var/log/docker-swarm.log 2>&1
         exit 0
