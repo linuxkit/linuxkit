df4eeaff80ff1216732346636a63cb38e320e9b0
Justin Cormack <justin.cormack@docker.com>
2016-07-12 13:34:49 +0100

    clean up moby script, add undo option
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- scripts/moby.sh
+++ scripts/moby.sh
@@ -2,11 +2,19 @@
 
 set -e
 
-cp alpine/initrd.img /Applications/Docker.app/Contents/Resources/moby/initrd.img
-cp alpine/kernel/vmlinuz64 /Applications/Docker.app/Contents/Resources/moby/vmlinuz64
+if [ "x$1" == "xundo" ]
+then
+	cp /Applications/Docker.app/Contents/Resources/moby/initrd.img- /Applications/Docker.app/Contents/Resources/moby/initrd.img
+	cp /Applications/Docker.app/Contents/Resources/moby/vmlinuz64- /Applications/Docker.app/Contents/Resources/moby/vmlinuz64
+else
+	cp /Applications/Docker.app/Contents/Resources/moby/initrd.img /Applications/Docker.app/Contents/Resources/moby/initrd.img-
+	cp /Applications/Docker.app/Contents/Resources/moby/vmlinuz64 /Applications/Docker.app/Contents/Resources/moby/vmlinuz64-
+	cp alpine/initrd.img /Applications/Docker.app/Contents/Resources/moby/initrd.img
+	cp alpine/kernel/vmlinuz64 /Applications/Docker.app/Contents/Resources/moby/vmlinuz64
+fi
 
 killall com.docker.driver.amd64-linux
 
-sleep 2
+sleep 20
 
 time docker ps
