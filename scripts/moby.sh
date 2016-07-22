d976eef44e35e4a99149f7bdad973724ced805d7
Natanael Copa <natanael.copa@docker.com>
2016-07-22 13:08:22 +0200

    Minor readabaility cleanup of scripts/moby.sh
    Use a variable $mobydir to make script more readable. No changes in
    functionality.
    
    Signed-off-by: Natanael Copa <natanael.copa@docker.com>

--- scripts/moby.sh
+++ scripts/moby.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+mobydir=/Applications/Docker.app/Contents/Resources/moby
+
 if [ "$1" = "undo" ]
 then
-	cp /Applications/Docker.app/Contents/Resources/moby/initrd.img- /Applications/Docker.app/Contents/Resources/moby/initrd.img
-	cp /Applications/Docker.app/Contents/Resources/moby/vmlinuz64- /Applications/Docker.app/Contents/Resources/moby/vmlinuz64
+	cp "$mobydir"/initrd.img- "$mobydir"/initrd.img
+	cp "$mobydir"/vmlinuz64- "$mobydir"/vmlinuz64
 else
-	cp /Applications/Docker.app/Contents/Resources/moby/initrd.img /Applications/Docker.app/Contents/Resources/moby/initrd.img-
-	cp /Applications/Docker.app/Contents/Resources/moby/vmlinuz64 /Applications/Docker.app/Contents/Resources/moby/vmlinuz64-
-	cp alpine/initrd.img /Applications/Docker.app/Contents/Resources/moby/initrd.img
-	cp alpine/kernel/x86_64/vmlinuz64 /Applications/Docker.app/Contents/Resources/moby/vmlinuz64
+	cp "$mobydir"/initrd.img "$mobydir"/initrd.img-
+	cp "$mobydir"/vmlinuz64 "$mobydir"/vmlinuz64-
+	cp alpine/initrd.img "$mobydir"/initrd.img
+	cp alpine/kernel/x86_64/vmlinuz64 "$mobydir"/vmlinuz64
 fi
 
 killall com.docker.driver.amd64-linux
