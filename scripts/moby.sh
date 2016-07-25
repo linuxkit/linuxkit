3cb758c4ae50b4d79cb716f7d37692f9ffeb5944
Natanael Copa <natanael.copa@docker.com>
2016-07-25 17:24:00 +0200

    Only backup vmlinuz/initrd once with moby script
    We probably want keep the backup of the original so don't overwrite
    backup if it exists.
    
    Signed-off-by: Natanael Copa <natanael.copa@docker.com>

--- scripts/moby.sh
+++ scripts/moby.sh
@@ -4,13 +4,20 @@ set -e
 
 mobydir=/Applications/Docker.app/Contents/Resources/moby
 
+backup_once() {
+	if ! [ -e "$1"- ]
+	then
+		cp "$1" "$1"-
+	fi
+}
+
 if [ "$1" = "undo" ]
 then
 	cp "$mobydir"/initrd.img- "$mobydir"/initrd.img
 	cp "$mobydir"/vmlinuz64- "$mobydir"/vmlinuz64
 else
-	cp "$mobydir"/initrd.img "$mobydir"/initrd.img-
-	cp "$mobydir"/vmlinuz64 "$mobydir"/vmlinuz64-
+	backup_once "$mobydir"/initrd.img
+	backup_once "$mobydir"/vmlinuz64
 	cp alpine/initrd.img "$mobydir"/initrd.img
 	cp alpine/kernel/x86_64/vmlinuz64 "$mobydir"/vmlinuz64
 fi
