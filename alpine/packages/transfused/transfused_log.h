31430016b2cdcbd4b7e5a67e17329d541b1c6cd4
Rolf Neugebauer <rolf.neugebauer@docker.com>
2016-09-02 16:57:52 +0100

    transfused: Use Linux coding style (mostly)
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

--- alpine/packages/transfused/transfused_log.h
+++ alpine/packages/transfused/transfused_log.h
@@ -1,27 +1,26 @@
+#ifndef _TRANSFUSED_LOG_H_
+#define _TRANSFUSED_LOG_H_
+
 #include <stdarg.h>
 #include <inttypes.h>
 
 #include "transfused.h"
 
-void die
-(int exit_code, parameters * params, const char * perror_arg,
- const char * fmt, ...);
+void die(int exit_code, parameters *params, const char *perror_arg,
+	 const char *fmt, ...);
 
-void vlog_locked
-(parameters * params, uint16_t msg_type, const char * fmt, va_list args);
+void vlog_locked(parameters *params, uint16_t msg_type,
+		 const char *fmt, va_list args);
+void vlog_time_locked(parameters *params, uint16_t msg_type,
+		      const char *fmt, va_list args);
 
-void vlog_time_locked
-(parameters * params, uint16_t msg_type, const char * fmt, va_list args);
+void log_time_locked(parameters *params, uint16_t msg_type,
+		     const char *fmt, ...);
 
-void log_time_locked
-(parameters * params, uint16_t msg_type, const char * fmt, ...);
+void log_time(parameters *params, const char *fmt, ...);
+void log_notice_time(parameters *params, const char *fmt, ...);
+void thread_log_time(connection_t *conn, const char *fmt, ...);
+void log_continue_locked(parameters *params, const char *fmt, ...);
+void log_continue(parameters *params, const char *fmt, ...);
 
-void log_time(parameters * params, const char * fmt, ...);
-
-void log_notice_time(parameters * params, const char * fmt, ...);
-
-void thread_log_time(connection_t * conn, const char * fmt, ...);
-
-void log_continue_locked(parameters * params, const char * fmt, ...);
-
-void log_continue(parameters * params, const char * fmt, ...);
+#endif /* _TRANSFUSED_LOG_H_ */
--- alpine/packages/transfused/transfused_vsock.c
+++ alpine/packages/transfused/transfused_vsock.c
@@ -7,93 +7,100 @@
 
 #include "transfused_log.h"
 
-long parse_cid(const char * address) {
-  char * end = NULL;
-  long cid = strtol(address, &end, 10);
-  if (address == end || *end != ':') {
-    *end = 0;
-    die(2, NULL, NULL, "Invalid vsock cid: %s", address);
-  }
-  return cid;
+long parse_cid(const char *address)
+{
+	char *end = NULL;
+	long cid = strtol(address, &end, 10);
+
+	if (address == end || *end != ':') {
+		*end = 0;
+		die(2, NULL, NULL, "Invalid vsock cid: %s", address);
+	}
+	return cid;
 }
 
-long parse_port(const char * port_str) {
-  char * end = NULL;
-  long port = strtol(port_str, &end, 10);
-  if (port_str == end || *end != '\0') {
-    *end = 0;
-    die(2, NULL, NULL, "Invalid vsock port: %s", port_str);
-  }
-  return port;
+long parse_port(const char *port_str)
+{
+	char *end = NULL;
+	long port = strtol(port_str, &end, 10);
+
+	if (port_str == end || *end != '\0') {
+		*end = 0;
+		die(2, NULL, NULL, "Invalid vsock port: %s", port_str);
+	}
+	return port;
 }
 
-int find_colon(const char * address) {
-  int colon = 0;
+int find_colon(const char *address)
+{
+	int colon = 0;
 
-  while (address[colon] != '\0')
-    if (address[colon] == ':') break;
-    else colon++;
+	while (address[colon] != '\0')
+		if (address[colon] == ':')
+			break;
+		else
+			colon++;
 
-  if (address[colon] == '\0')
-    die(2, NULL, NULL, "Missing port in vsock address %s", address);
+	if (address[colon] == '\0')
+		die(2, NULL, NULL, "Missing port in vsock address %s", address);
 
-  return colon;
+	return colon;
 }
 
-int bind_vsock(const char * address) {
-  long cid, port;
-  int colon;
+int bind_vsock(const char *address)
+{
+	long cid, port;
+	int colon;
+	struct sockaddr_vm sa_listen = {
+		.svm_family = AF_VSOCK,
+	};
+	int sock_fd;
 
-  struct sockaddr_vm sa_listen = {
-    .svm_family = AF_VSOCK,
-  };
+	colon = find_colon(address);
 
-  int sock_fd;
+	if (address[0] == '_' && colon == 1)
+		cid = VMADDR_CID_ANY;
+	else
+		cid = parse_cid(address);
 
-  colon = find_colon(address);
+	port = parse_port(address + colon + 1);
 
-  if (address[0] == '_' && colon == 1) cid = VMADDR_CID_ANY;
-  else cid = parse_cid(address);
+	sa_listen.svm_cid = cid;
+	sa_listen.svm_port = port;
 
-  port = parse_port(address + colon + 1);
+	sock_fd = socket(AF_VSOCK, SOCK_STREAM, 0);
+	if (sock_fd < 0)
+		die(1, NULL, "socket(AF_VSOCK)", "");
 
-  sa_listen.svm_cid = cid;
-  sa_listen.svm_port = port;
+	if (bind(sock_fd, (struct sockaddr *)&sa_listen, sizeof(sa_listen)))
+		die(1, NULL, "bind(AF_VSOCK)", "");
 
-  sock_fd = socket(AF_VSOCK, SOCK_STREAM, 0);
-  if (sock_fd < 0)
-    die(1, NULL, "socket(AF_VSOCK)", "");
-
-  if (bind(sock_fd, (struct sockaddr *) &sa_listen, sizeof(sa_listen)))
-    die(1, NULL, "bind(AF_VSOCK)", "");
-
-  return sock_fd;
+	return sock_fd;
 }
 
-int connect_vsock(const char * address) {
-  long cid, port;
-  int colon;
+int connect_vsock(const char *address)
+{
+	long cid, port;
+	int colon;
+	struct sockaddr_vm sa_connect = {
+		.svm_family = AF_VSOCK,
+	};
+	int sock_fd;
 
-  struct sockaddr_vm sa_connect = {
-    .svm_family = AF_VSOCK,
-  };
+	colon = find_colon(address);
 
-  int sock_fd;
+	cid = parse_cid(address);
+	port = parse_port(address + colon + 1);
 
-  colon = find_colon(address);
+	sa_connect.svm_cid = cid;
+	sa_connect.svm_port = port;
 
-  cid = parse_cid(address);
-  port = parse_port(address + colon + 1);
+	sock_fd = socket(AF_VSOCK, SOCK_STREAM, 0);
+	if (sock_fd < 0)
+		die(1, NULL, "socket(AF_VSOCK)", "");
 
-  sa_connect.svm_cid = cid;
-  sa_connect.svm_port = port;
+	if (connect(sock_fd, (struct sockaddr *)&sa_connect, sizeof(sa_connect)))
+		die(1, NULL, "connect(AF_VSOCK)", "");
 
-  sock_fd = socket(AF_VSOCK, SOCK_STREAM, 0);
-  if (sock_fd < 0)
-    die(1, NULL, "socket(AF_VSOCK)", "");
-
-  if (connect(sock_fd, (struct sockaddr *) &sa_connect, sizeof(sa_connect)))
-    die(1, NULL, "connect(AF_VSOCK)", "");
-
-  return sock_fd;
+	return sock_fd;
 }
