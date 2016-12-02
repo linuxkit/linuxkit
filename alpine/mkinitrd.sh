72d4d5aefc670c9fa2c861f0a7a54f1d6d73fc01
Justin Cormack <justin.cormack@docker.com>
2016-12-02 14:51:03 +0000

    Align compressed initrd to 4 bytes
    Allows appending another initrd.
    
    Also build initrd on tmpfs as should be a bit faster now we have to do
    another copy.
    
    Fix #618
    
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- alpine/mkinitrd.sh
+++ alpine/mkinitrd.sh
@@ -32,7 +32,31 @@ EOF
 
 printf 'moby' > /tmp/etc/hostname
 
-rm /tmp/mkinitrd.sh
-
 cd /tmp
-find . | cpio -H newc -o | gzip -9
+
+rm mkinitrd.sh
+
+find . | cpio -H newc -o | gzip -9 > /initrd/initrd.img
+
+cd /initrd
+
+SIZE=$(stat -c "%s" initrd.img)
+SIZE4=$(( $SIZE / 4 \* 4 ))
+DIFF=$(( $SIZE - $SIZE4 ))
+[ $DIFF -ne 0 ] && DIFF=$(( 4 - $DIFF ))
+
+dd if=/dev/zero bs=1 count=$DIFF of=zeropad
+
+cat zeropad >> initrd.img
+
+SIZE=$(stat -c "%s" initrd.img)
+SIZE4=$(( $SIZE / 4 \* 4 )) 
+DIFF=$(( $SIZE - $SIZE4 ))
+
+if [ $DIFF -ne 0 ]
+then
+	echo "Bad aligment" >2
+	exit 1
+fi
+
+cat initrd.img
