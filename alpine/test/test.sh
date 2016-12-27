00b3924067bca4bd2cdb36e7678e5b356a570382
Justin Cormack <justin.cormack@docker.com>
2016-12-27 23:13:13 +0000

    Update base images to Alpine 3.5
    excludes
    - Go base images as not yet available
    - perf which does not build with latest image
    
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- alpine/test/test.sh
+++ alpine/test/test.sh
@@ -10,4 +10,4 @@ docker run --rm alpine true
 docker pull armhf/alpine
 docker run --rm armhf/alpine uname -a
 docker swarm init
-docker run mobylinux/check-config:bc2b57a0770129c75a6676ae0c944ece1d50cc3f
+docker run mobylinux/check-config@sha256:7f8327e9eeae67a7f1388ad0ac5089454fea0636c175feb4400e5fc76ebc816e
