ea612505f6307d3be3e759440839bcb9540d065a
Justin Cormack <justin.cormack@docker.com>
2017-01-11 16:49:59 +0000

    Adjust kernel config tests to support 4.4
    - some options are different
    
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- alpine/test/test.sh
+++ alpine/test/test.sh
@@ -11,4 +11,4 @@ docker pull armhf/alpine
 docker run --rm armhf/alpine uname -a
 docker swarm init
 docker run mobylinux/check-config@sha256:4282f589d5a72004c3991c0412e45ba0ab6bb8c0c7d97dc40dabc828700e99ab
-docker run mobylinux/check-kernel-config@sha256:beabc0fd77bb9562a03104eecb34286d5aa695896e0d3e56b36876b24d2a9126
+docker run mobylinux/check-kernel-config@sha256:8c48a0f8456e1e5027eb2540974e6304d8cf58263c7553b61838cb627c4e790c
