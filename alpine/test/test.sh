1b3186cd35e1c0fe35e5f2fb7ed16c0af20c4605
Justin Cormack <justin.cormack@docker.com>
2017-01-13 16:56:08 +0000

    Tidy up test build
    Do not build from Go image as not clear we will switch it.
    
    Print some output at the end for a quick visual success indicator.
    
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- alpine/test/test.sh
+++ alpine/test/test.sh
@@ -12,3 +12,4 @@ docker run --rm armhf/alpine uname -a
 docker swarm init
 docker run mobylinux/check-config@sha256:4282f589d5a72004c3991c0412e45ba0ab6bb8c0c7d97dc40dabc828700e99ab
 docker run mobylinux/check-kernel-config@sha256:8c48a0f8456e1e5027eb2540974e6304d8cf58263c7553b61838cb627c4e790c
+cat /etc/moby
