f5954fbf5e118f166af137ebc423d58801fdf598
Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>
2017-01-23 13:14:19 -0800

    Merge lint.sh into compile.sh, remove unused alpine-build-go
    Signed-off-by: Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>

--- alpine/base/go-compile/compile.sh
+++ alpine/base/go-compile/compile.sh
@@ -42,7 +42,17 @@ mkdir -p $dir
 # untar input
 tar xf - -C $dir
 
-/usr/bin/lint.sh $dir
+cd $dir
+
+# lint before building
+>&2 echo "gofmt..."
+test -z $(gofmt -s -l .| grep -v .pb. | grep -v */vendor/ | tee /dev/stderr)
+
+>&2 echo "govet..."
+test -z $(go tool vet -printf=false . 2>&1 | grep -v */vendor/ | tee /dev/stderr)
+
+>&2 echo "golint..."
+test -z $(find . -type f -name "*.go" -not -path "*/vendor/*" -not -name "*.pb.*" -exec golint {} \; | tee /dev/stderr)
 
 >&2 echo "go build..."
 
