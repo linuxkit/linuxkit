0276d5234011c22128ac0e9f2f8d36fe4812fa0a
French Ben <frenchben@docker.com>
2017-02-20 20:49:47 -0800

    Removed extra cloud make
    Signed-off-by: French Ben <frenchben@docker.com>

--- alpine/cloud/aws/aws.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./bake-ami.sh "$@" 1>&2
-if [ "$1" = "bake" ]
-then
-	cat /build/ami_id.out
-fi
--- alpine/cloud/aws/joiner-user-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-logcmd()
-{
-	"$@" 2>&1 | awk -v timestamp="$(date) " '$0=timestamp$0' >>/var/log/docker-swarm.log
-}
-
-logcmd docker swarm join {{MANAGER_IP}}:4500
-logcmd docker swarm info
