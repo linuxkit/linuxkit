56b7be6c254e58b109267c8b2db1033779fd7961
Rolf Neugebauer <rolf.neugebauer@docker.com>
2017-04-07 11:48:03 +0100

    demo: Add script to start infrakit
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

---
+++ projects/demo/etcd/start-infrakit.sh
@@ -0,0 +1,18 @@
+# !/bin/sh
+
+# Start the infrakit plugins, save their PID
+
+INFRAKIT_HOME=~/.infrakit
+IK_PLUGINS=$INFRAKIT_HOME/plugins
+
+rm -rf $INFRAKIT_HOME
+
+infrakit-flavor-vanilla &
+infrakit-instance-hyperkit &
+infrakit-instance-gcp --project docker4x --zone europe-west1-d &
+
+# start the group plugin in the foreground. If it exits, it will take
+# the others down as well.
+infrakit-group-default
+
+rm -rf $INFRAKIT_HOME
