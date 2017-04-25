60c632dc355fa2b667ba0a8daa457c508849d7b2
Tiago Pires <tandrepires@gmail.com>
2017-04-25 22:56:05 +0100

    Use linuxkit on boot scripts instead of moby
    Fixes #1711
    
    Signed-off-by: Tiago Pires <tandrepires@gmail.com>

--- projects/kubernetes/boot-node.sh
+++ projects/kubernetes/boot-node.sh
@@ -5,4 +5,4 @@ shift
 disk="kube-${name}-disk.img"
 set -x
 rm -f "${disk}"
-../../bin/moby run hyperkit -cpus 2 -mem 4096 -disk-size 4096 -disk "${disk}" -data "${*}" kube-node
+../../bin/linuxkit run hyperkit -cpus 2 -mem 4096 -disk-size 4096 -disk "${disk}" -data "${*}" kube-node
