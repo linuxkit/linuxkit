2493d63fb89d693001a9d27a9c6469c80e2d6ca1
radu-matei <matei.radu94@gmail.com>
2017-05-18 10:58:47 -0700

    Add VM creation

--- src/cmd/linuxkit/run_azure.go
+++ src/cmd/linuxkit/run_azure.go
@@ -6,9 +6,26 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/Azure/azure-sdk-for-go/arm/compute"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/adal"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/autorest/to"
 )
 
+// This program requires that the following environment vars are set:
+
+// AZURE_TENANT_ID: contains your Azure Active Directory tenant ID or domain
+// AZURE_SUBSCRIPTION_ID: contains your Azure Subscription ID
+// AZURE_CLIENT_ID: contains your Azure Active Directory Application Client ID
+// AZURE_CLIENT_SECRET: contains your Azure Active Directory Application Secret
+
 const ()
+
+var (
+	vmClient compute.VirtualMachinesClient
+)
 
 // Process the run arguments and execute run
 func runAzure(args []string) {
@@ -23,9 +40,87 @@ func runAzure(args []string) {
 		flags.PrintDefaults()
 	}
 
-	vhdUri := flags.Parse()
+	subscriptionID := getEnvVarOrExit("AZURE_SUBSCRIPTION_ID")
+	tenantID := getEnvVarOrExit("AZURE_TENANT_ID")
+
+	clientID := getEnvVarOrExit("AZURE_CLIENT_ID")
+	clientSecret := getEnvVarOrExit("AZURE_CLIENT_SECRET")
+
+	resourceGroup := flags.String("resourceGroup", "", "Name of resource group to be used for VM")
+	vmName := flags.String("vmName", "default-linuxkit-vm", "Name of the Azure VM")
+	vhdURI := flags.String("vhdUri", "", "Address of the VHD to be used as image for VM")
+	location := flags.String("location", "westus", "Location of the VM")
 
 	if err := flags.Parse(args); err != nil {
 		log.Fatal("Unable to parse args")
 	}
+
+	vmClient := getVMClient(subscriptionID, tenantID, clientID, clientSecret)
+	vmClient.CreateOrUpdate(*resourceGroup, *vmName, setVMParameters(*location, *vhdURI), nil)
+
 }
+
+func getEnvVarOrExit(varName string) string {
+	value := os.Getenv(varName)
+	if value == "" {
+		fmt.Printf("Missing environment variable %s\n", varName)
+		os.Exit(1)
+	}
+
+	return value
+}
+
+func getVMClient(subscriptionID, tenantID, clientID, clientSecret string) compute.VirtualMachinesClient {
+	oAuthConfig, err := adal.NewOAuthConfig(azure.PublicCloud.ActiveDirectoryEndpoint, tenantID)
+	if err != nil {
+		fmt.Printf(err.Error())
+		log.Fatalf("Cannot get oAuth configuration")
+	}
+
+	token, err := adal.NewServicePrincipalToken(*oAuthConfig, clientID, clientSecret, azure.PublicCloud.ResourceManagerEndpoint)
+	if err != nil {
+		fmt.Printf(err.Error())
+		log.Fatalf("Cannot get service principal token")
+	}
+
+	vmClient = compute.NewVirtualMachinesClient(subscriptionID)
+	vmClient.Authorizer = autorest.NewBearerAuthorizer(token)
+
+	return vmClient
+}
+
+func setVMParameters(location, vhdURI string) compute.VirtualMachine {
+	return compute.VirtualMachine{
+		Location: &location,
+		VirtualMachineProperties: &compute.VirtualMachineProperties{
+			StorageProfile: &compute.StorageProfile{
+				OsDisk: &compute.OSDisk{
+					Name:         to.StringPtr("os-disk"),
+					CreateOption: compute.FromImage,
+					OsType:       compute.Linux,
+					Image: &compute.VirtualHardDisk{
+						URI: &vhdURI,
+					},
+				},
+			},
+		},
+	}
+}
+
+/*
+func setVMParameters(location, vhdURI string) compute.VirtualMachine {
+	return compute.VirtualMachine{
+		Location: &location,
+		VirtualMachineProperties: &compute.VirtualMachineProperties{
+			StorageProfile: &compute.StorageProfile{
+				OsDisk: &compute.OSDisk{
+					Name: to.StringPtr("os-disk"),
+					Image: &compute.VirtualHardDisk{
+						URI: &vhdURI,
+					},
+				},
+			},
+		},
+	}
+}
+*/
