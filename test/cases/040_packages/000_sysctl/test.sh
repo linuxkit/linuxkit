345a294b5e88a538fc5f893dbbd7bec0e0cd1ac6
Justin Cormack <justin.cormack@docker.com>
2017-05-19 15:41:13 +0100

    Add a test case for the sysctl package
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

---
+++ test/cases/040_packages/000_sysctl/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# SUMMARY: Check that the sysctl config works
+# LABELS:
+# REPEAT:
+# AUTHOR: Justin Cormack <justin.cormack@docker.com>
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+clean_up() {
+	find . -iname "test-sysctl*" -not -iname "*.yml" -exec rm -rf {} \;
+}
+trap clean_up EXIT
+
+# Test code goes here
+moby build test-sysctl
+RESULT="$(linuxkit run qemu test-sysctl)"
+echo "${RESULT}" | grep -q "suite PASSED"
+
+exit 0
---
+++ test/cases/040_packages/group.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# SUMMARY: LinuxKit package tests
+# LABELS:
+# For the top level group.sh also specify a 'NAME:' comment
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+#. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+
+group_init() {
+    # Group initialisation code goes here
+    return 0
+}
+
+group_deinit() {
+    # Group de-initialisation code goes here
+    return 0
+}
+
+CMD=$1
+case $CMD in
+init)
+    group_init
+    res=$?
+    ;;
+deinit)
+    group_deinit
+    res=$?
+    ;;
+*)
+    res=1
+    ;;
+esac
+
+exit $res
