e1920b4f221c42ebcf3335a855a8024eadf41617
Rolf Neugebauer <rolf.neugebauer@docker.com>
2017-05-19 11:34:19 +0100

    tests: Add a test for running qemu in a container
    The qemu backend defaults to running against a locally installed
    qemu but falls back to running in a container if qemu is not
    installed. This test explicitly tests that the containerised
    qemu backend works only if there is a locally installed qemu.
    
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

---
+++ test/cases/010_platforms/000_qemu/100_container/test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# SUMMARY: Check that qemu runs containerised
+# LABELS:
+# AUTHOR: Rolf Neugebauer <rolf.neugebauer@docker.com>
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+NAME=qemu-kernel
+
+clean_up() {
+	# remove any files, containers, images etc
+	rm -rf ${NAME}* || true
+}
+
+trap clean_up EXIT
+
+# check if qemu is installed locally
+QEMU=$(command -v qemu-system-x86_64 || true)
+if [ -z "${QEMU}" ]; then
+    # No qemu installed so don't bother to test as all the other
+    # qemu tests would have been run containerised
+    echo "No locally installed qemu"
+    exit $RT_CANCEL
+fi
+
+moby build -name "${NAME}" test.yml
+[ -f "${NAME}-kernel" ] || exit 1
+[ -f "${NAME}-initrd.img" ] || exit 1
+[ -f "${NAME}-cmdline" ]|| exit 1
+linuxkit run qemu -containerized "${NAME}" | grep -q "Welcome to LinuxKit"
+exit 0
