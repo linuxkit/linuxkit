5394ec26f2b302cf529d6184eb7fbf38ee058f55
Justin Cormack <justin.cormack@docker.com>
2017-06-01 14:49:49 +0100

    Update mkimage tests to match output
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- test/cases/040_packages/013_mkimage/test.sh
+++ test/cases/040_packages/013_mkimage/test.sh
@@ -12,14 +12,15 @@ set -e
 clean_up() {
 	find . -iname "run*" -not -iname "*.yml" -exec rm -rf {} \;
 	find . -iname "mkimage*" -not -iname "*.yml" -exec rm -rf {} \;
-	rm -f disk.qcow2
+	rm -f disk.qcow2 tarball.img
 }
 trap clean_up EXIT
 
 # Test code goes here
 moby build -output kernel+initrd run.yml
 moby build -output kernel+initrd mkimage.yml
-linuxkit run qemu -disk disk.qcow2,size=200M,format=qcow2 -kernel mkimage
+tar cf tarball.img run-kernel run-initrd.img run-cmdline
+linuxkit run qemu -disk disk.qcow2,size=200M,format=qcow2 -disk tarball.img,format=raw -kernel mkimage
 linuxkit run qemu disk.qcow2
 
 exit 0
