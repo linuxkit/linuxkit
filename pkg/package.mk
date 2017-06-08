385490345bdfb0c0db7be27fdf16bfde02c6cb36
Dave Tucker <dt@docker.com>
2017-06-08 15:39:08 +0100

    Makefile: Add hash and check targets for pkgs
    'check' will check if the package has been pushed to hub
    'hash' prints the image name, organisation and tag/hash
    
    These targets are now called from the top-level Makefile.
    This will check if the packages are pushed to hub.
    If not, it will build them locally
    
    Signed-off-by: Dave Tucker <dt@docker.com>

--- pkg/package.mk
+++ pkg/package.mk
@@ -2,8 +2,17 @@
 default: push
 
 ORG?=linuxkit
+STAGE_ORG?=linuxkit.datakit.ci:5000
 HASH?=$(shell git ls-tree HEAD -- ../$(notdir $(CURDIR)) | awk '{print $$3}')
 BASE_DEPS=Dockerfile Makefile
+OVERRIDES?=override.yml
+
+UNAME_S := $(shell uname -s)
+ifeq ($(UNAME_S),Linux)
+	TIMEOUT=timeout
+else
+	TIMEOUT=gtimeout
+endif
 
 tag: $(BASE_DEPS) $(DEPS)
 ifndef $(NETWORK)
@@ -15,3 +24,11 @@ endif
 push: tag
 	DOCKER_CONTENT_TRUST=1 docker pull $(ORG)/$(IMAGE):$(HASH) || \
 	DOCKER_CONTENT_TRUST=1 docker push $(ORG)/$(IMAGE):$(HASH)
+
+hash:
+	@echo "$(ORG)/$(IMAGE):$(HASH)"
+
+check:
+	@$(TIMEOUT) 5 notary -s https://notary.docker.io lookup docker.io/$(ORG)/$(IMAGE) $(HASH) > /dev/null || \
+	$(MAKE) ORG=$(STAGE_ORG) push && \
+	echo "- source: $(ORG)/$(IMAGE)\n  subsitute: $(STAGE_ORG)/$(IMAGE):$(HASH)\n" >> $(OVERRIDES)
