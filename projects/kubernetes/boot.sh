c47b2c770c461713fb5fdbc44787d6fbe803f56f
Ian Campbell <ian.campbell@docker.com>
2017-06-13 10:28:12 +0100

    kubernetes: Create disk in state dir
    Signed-off-by: Ian Campbell <ian.campbell@docker.com>

--- projects/kubernetes/boot.sh
+++ projects/kubernetes/boot.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -eu
 if [ $# -eq 0 ] ; then
     img="kube-master"
-    disk="kube-master-disk.img"
     data=""
     state="kube-master-state"
 elif [ $# -gt 1 ] ; then
     img="kube-node"
     name="node-${1}"
     shift
-    disk="kube-${name}-disk.img"
     data="${*}"
     state="kube-${name}-state"
 else
@@ -20,6 +18,5 @@ else
     exit 1
 fi
 set -x
-rm -f "${disk}"
 rm -rf "${state}"
-../../bin/linuxkit run -cpus 2 -mem 4096 -state "${state}" -disk "${disk}",size=4G -data "${data}" "${img}"
+../../bin/linuxkit run -cpus 2 -mem 4096 -state "${state}" -disk size=4G -data "${data}" "${img}"
