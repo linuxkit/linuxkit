1dbec1ef30b9d3c61e09e817d813ce5db12434d3
Ian Campbell <ijc@docker.com>
2017-07-24 12:28:45 +0100

    kubernetes: inline ssh.sh into ssh_into_kubelet.sh
    Signed-off-by: Ian Campbell <ijc@docker.com>

--- projects/kubernetes/ssh_into_kubelet.sh
+++ projects/kubernetes/ssh_into_kubelet.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -eux
-./ssh.sh -t root@"$1" ctr exec --tty --exec-id ssh kubelet ash -l
+ssh="docker run --rm -ti \
+  -v $HOME/.ssh/:/root/.ssh \
+    jdeathe/centos-ssh \
+	-o LogLevel=FATAL \
+	-o StrictHostKeyChecking=no \
+	-o UserKnownHostsFile=/dev/null \
+	-o IdentitiesOnly=yes"
+$ssh -t root@"$1" ctr exec --tty --exec-id ssh kubelet ash -l
