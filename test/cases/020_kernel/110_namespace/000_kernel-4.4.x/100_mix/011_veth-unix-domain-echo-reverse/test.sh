be71dde3f0d0e6e6af06218b85a2421ecd8bc5d4
Rolf Neugebauer <rolf.neugebauer@docker.com>
2017-07-24 18:32:54 +0100

    tests: Add namespace stress tests
    These tests run individual and a mix of namespace stress tests
    mostly around networking and unix domain sockets where either
    the client or the server of socket echo application is run inside
    a container in different configurations:
    - different protocols
    - short or long lived connections
    - different levels of concurrency
    
    Tests are only run if the 'kernel' label is specified and more
    detailed tests are run if the additional 'kernel-extra' label
    is specified.
    
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

---
+++ test/cases/020_kernel/110_namespace/000_kernel-4.4.x/100_mix/011_veth-unix-domain-echo-reverse/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# SUMMARY: Namespace stress with a mix of concurrent TCP/UDP/IPv4/IPv6 and unix domain socket connections in reverse direction
+# LABELS:
+# REPEAT:
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+clean_up() {
+    find . -depth -iname "test-ns*" -not -iname "*.yml" -exec rm -rf {} \;
+}
+trap clean_up EXIT
+
+# Test code goes here
+moby build -output kernel+initrd test-ns.yml
+RESULT="$(linuxkit run -cpus 2 test-ns)"
+echo "${RESULT}" | grep -q "suite PASSED"
+
+exit 0
---
+++ test/cases/020_kernel/110_namespace/003_kernel-4.11.x/010_veth/016_echo-tcp-ipv4-long-10con-single-reverse/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# SUMMARY: Namespace stress with 10 concurrent long running TCP/IPv4 connections over a veth pair in reverse order
+# LABELS:
+# REPEAT:
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+clean_up() {
+    find . -depth -iname "test-ns*" -not -iname "*.yml" -exec rm -rf {} \;
+}
+trap clean_up EXIT
+
+# Test code goes here
+moby build -output kernel+initrd test-ns.yml
+RESULT="$(linuxkit run -cpus 2 test-ns)"
+echo "${RESULT}" | grep -q "suite PASSED"
+
+exit 0
