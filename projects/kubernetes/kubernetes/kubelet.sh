1074b4a8d4a235987a3b4c1cb89f301086fb6c67
Ian Campbell <ijc@docker.com>
2017-09-13 17:01:31 +0100

    kubernetes: point kubelet at /var/lib/cni directly
    Still need the /opt bind to host for weave.yaml but no need for etc any longer.
    
    Signed-off-by: Ian Campbell <ijc@docker.com>

--- projects/kubernetes/kubernetes/kubelet.sh
+++ projects/kubernetes/kubernetes/kubelet.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 mount --bind /opt/cni /var/lib/cni/opt
-mount --bind /etc/cni /var/lib/cni/etc
 until kubelet --kubeconfig=/var/lib/kubeadm/kubelet.conf \
 	      --require-kubeconfig=true \
 	      --pod-manifest-path=/var/lib/kubeadm/manifests \
@@ -10,8 +9,8 @@ until kubelet --kubeconfig=/var/lib/kubeadm/kubelet.conf \
 	      --cgroups-per-qos=false \
 	      --enforce-node-allocatable= \
 	      --network-plugin=cni \
-	      --cni-conf-dir=/etc/cni/net.d \
-	      --cni-bin-dir=/opt/cni/bin \
+	      --cni-conf-dir=/var/lib/cni/etc/net.d \
+	      --cni-bin-dir=/var/lib/cni/opt/bin \
 	      $@; do
     if [ ! -f /var/config/userdata ] ; then
 	sleep 1
