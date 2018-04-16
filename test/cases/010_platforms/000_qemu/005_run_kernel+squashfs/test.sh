c1447193b3eba486851a23be1e91408315d96964
Rolf Neugebauer <rolf.neugebauer@docker.com>
2018-04-16 18:27:43 +0100

    tests: Add build/qemu/hyperkit tests for kernel+squashfs
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@docker.com>

---
+++ test/cases/010_platforms/000_qemu/005_run_kernel+squashfs/test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# SUMMARY: Check that the kernel+squashfs image boots in qemu
+# LABELS:
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+NAME=qemu-squashfs
+
+clean_up() {
+	rm -rf ${NAME}-*
+}
+trap clean_up EXIT
+
+linuxkit build -format kernel+squashfs -name "${NAME}" test.yml
+[ -f "${NAME}-kernel" ] || exit 1
+[ -f "${NAME}-squashfs.img" ] || exit 1
+[ -f "${NAME}-cmdline" ]|| exit 1
+linuxkit run qemu -squashfs "${NAME}" | grep -q "Welcome to LinuxKit"
+
+exit 0
