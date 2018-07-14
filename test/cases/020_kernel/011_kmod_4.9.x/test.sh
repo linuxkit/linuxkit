e3369b11a66b58c7a1e67ebec04033a4baf35436
Rolf Neugebauer <rolf.neugebauer@gmail.com>
2018-07-14 14:45:55 +0100

    Update YAMLs to latest kernels
    Signed-off-by: Rolf Neugebauer <rolf.neugebauer@gmail.com>

--- test/cases/020_kernel/011_kmod_4.9.x/test.sh
+++ test/cases/020_kernel/011_kmod_4.9.x/test.sh
@@ -19,7 +19,7 @@ clean_up() {
 trap clean_up EXIT
 
 # Make sure we have the latest kernel image
-docker pull linuxkit/kernel:4.9.111
+docker pull linuxkit/kernel:4.9.112
 # Build a package
 docker build -t ${IMAGE_NAME} .
 
