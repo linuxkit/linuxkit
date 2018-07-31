6e48041dee742b811cc1ec5262d285ca5fcae065
Krister Johansen <krister.johansen@oracle.com>
2018-07-30 21:37:33 -0700

    Add a test case for bcc to packages.
    Signed-off-by: Krister Johansen <krister.johansen@oracle.com>

---
+++ test/cases/040_packages/032_bcc/test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# SUMMARY: Check that the bcc package works
+# LABELS: skip
+# REPEAT:
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+NAME=bcc
+
+clean_up() {
+	rm -rf ${NAME}-*
+}
+trap clean_up EXIT
+
+# Test code goes here
+linuxkit build -format kernel+initrd -name "${NAME}" test.yml
+RESULT="$(linuxkit run ${NAME})"
+echo "${RESULT}"
+echo "${RESULT}" | grep -q "suite PASSED"
+
+exit 0
