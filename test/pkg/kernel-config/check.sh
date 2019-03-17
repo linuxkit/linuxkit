25e25da424f419e52699c56618b369142f390d6a
Rolf Neugebauer <rn@rneugeba.io>
2019-03-17 11:56:46 -0700

    tests: Skip Moby kernel config checks on 5.x
    See https://github.com/moby/moby/issues/38887
    for details. Basically 5.x removed support for
    CFQ with f382fb0bcef4 ("block: remove legacy IO
    schedulers") and the Moby check still requires it.
    
    Signed-off-by: Rolf Neugebauer <rn@rneugeba.io>

--- test/pkg/kernel-config/check.sh
+++ test/pkg/kernel-config/check.sh
@@ -6,6 +6,13 @@ function failed {
 }
 
 /check-kernel-config.sh || failed
-bash /check-config.sh || failed
+
+# Skip moby kernel checks on 5.x kernels for now.
+# See: https://github.com/moby/moby/issues/38887
+kernelVersion="$(uname -r)"
+kernelMajor="${kernelVersion%%.*}"
+if [ "$kernelMajor" -lt 5 ]; then
+    bash /check-config.sh || failed
+fi
 
 printf "Kernel config test suite PASSED\n"
