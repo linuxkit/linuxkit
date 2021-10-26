10599f776ab776cca1547ffbd1934d4128f605e4
David Scott <dave@recoil.org>
2021-10-26 20:07:38 +0100

    test: add a case for cgroupv2
    Signed-off-by: David Scott <dave@recoil.org>

---
+++ test/cases/040_packages/003_cgroupv2/check.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -x
+
+function failed {
+	printf "cgroup not detected, suite FAILED\n" >&1
+	exit 1
+}
+
+DEVICE=/dev/sdb
+
+mount | grep cgroup2 || failed
+
+printf "cgroup2 detected, suite PASSED\n" >&1
---
+++ test/cases/040_packages/003_cgroupv2/test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# SUMMARY: Run containerd test
+# LABELS:
+# REPEAT:
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+NAME=cgroupv2
+
+clean_up() {
+	rm -rf ${NAME}-*
+}
+trap clean_up EXIT
+
+# Test code goes here
+linuxkit build -format kernel+initrd -name "${NAME}" test.yml
+RESULT="$(linuxkit run -mem 3072 ${NAME})"
+echo "${RESULT}" | grep -q "suite PASSED"
+
+exit 0
