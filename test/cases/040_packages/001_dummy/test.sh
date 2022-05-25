857f53cdff9dde76679833b0632fd3dfcdc6e0e5
Avi Deitcher <avi@deitcher.net>
2022-05-25 08:09:15 -1000

    test pkg/dummy
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

---
+++ test/cases/040_packages/001_dummy/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# SUMMARY: Check that the dummy pkg exists
+# LABELS:
+# REPEAT:
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+NAME=dummy
+
+clean_up() {
+	rm -rf ${NAME}-*
+}
+trap clean_up EXIT
+
+# Test code goes here
+linuxkit build -format kernel+initrd -name "${NAME}" test.yml
+# all we are checking is that we can build using the dummy package, which
+# was not pushed out
+
+exit 0
