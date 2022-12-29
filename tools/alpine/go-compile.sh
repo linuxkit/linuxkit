3e790399f5b5d912dbd7cf9ca1090faa775edb47
Avi Deitcher <avi@deitcher.net>
2022-12-29 10:55:56 +0200

    Update linuxkit/alpine
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- tools/alpine/go-compile.sh
+++ tools/alpine/go-compile.sh
@@ -18,16 +18,16 @@ cd "$dir"
 
 # lint before building
 >&2 echo "gofmt..."
-test -z $(gofmt -s -l .| grep -v .pb. | grep -v vendor/ | tee /dev/stderr)
+test -z "$(gofmt -s -l .| grep -v .pb. | grep -v vendor/ | tee /dev/stderr)"
 
 >&2 echo "govet..."
-test -z $(GOOS=linux go vet -printf=false . 2>&1 | grep -v "^#" | grep -v vendor/ | tee /dev/stderr)
+test -z "$(GOOS=linux go vet -printf=false . 2>&1 | grep -v "^#" | grep -v vendor/ | tee /dev/stderr)"
 
 >&2 echo "golint..."
-test -z $(find . -type f -name "*.go" -not -path "*/vendor/*" -not -name "*.pb.*" -exec golint {} \; | tee /dev/stderr)
+test -z "$(find . -type f -name "*.go" -not -path "*/vendor/*" -not -name "*.pb.*" -exec golint {} \; | tee /dev/stderr)"
 
 >&2 echo "ineffassign..."
-test -z $(find . -type f -name "*.go" -not -path "*/vendor/*" -not -name "*.pb.*" -exec ineffassign {} \; | tee /dev/stderr)
+test -z "$(find . -type f -name "*.go" -not -path "*/vendor/*" -not -name "*.pb.*" -exec ineffassign {} \; | tee /dev/stderr)"
 
 >&2 echo "go test..."
 go test
