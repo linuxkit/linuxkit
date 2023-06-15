1b22e6f194ebd8ce745bc0b0ed3c017fd82c44c0
Avi Deitcher <avi@deitcher.net>
2023-06-15 21:33:34 +0300

    replace complex manifest-tool with straight go-containerregistry
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- src/cmd/linuxkit/pkglib/index.go
+++ src/cmd/linuxkit/pkglib/index.go
@@ -3,6 +3,8 @@ package pkglib
 import (
 	"fmt"
 
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/linuxkit/linuxkit/src/cmd/linuxkit/registry"
 	"github.com/linuxkit/linuxkit/src/cmd/linuxkit/util"
 )
@@ -16,13 +18,15 @@ func (p Pkg) Index(bos ...BuildOpt) error {
 		}
 	}
 	name := p.FullTag()
+	options := []remote.Option{remote.WithAuthFromKeychain(authn.DefaultKeychain)}
 
 	// Even though we may have pushed the index, we want to be sure that we have an index that includes every architecture on the registry,
-	// not just those that were in our local cache. So we use manifest-tool library to build a broad index
+	// not just those that were in our local cache. So we call PushManifest to push an index that includes all arch-specific images
+	// already in the registry.
 
 	// push based on tag
 	fmt.Printf("Pushing index based on all arch-specific images in registry %s\n", name)
-	_, _, err := registry.PushManifest(name)
+	_, _, err := registry.PushManifest(name, options...)
 	if err != nil {
 		return err
 	}
@@ -36,7 +40,7 @@ func (p Pkg) Index(bos ...BuildOpt) error {
 		fullRelTag := util.ReferenceExpand(relTag)
 
 		fmt.Printf("Pushing index based on all arch-specific images in registry %s\n", fullRelTag)
-		_, _, err = registry.PushManifest(fullRelTag)
+		_, _, err = registry.PushManifest(fullRelTag, options...)
 		if err != nil {
 			return err
 		}
