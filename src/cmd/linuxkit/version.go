8bb9174b28f39ea46047afc90406eb7737087385
Avi Deitcher <avi@deitcher.net>
2024-07-11 14:26:51 +0200

    properly save linuxkit version
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- src/cmd/linuxkit/version.go
+++ src/cmd/linuxkit/version.go
@@ -10,11 +10,22 @@ import (
 )
 
 func versionCmd() *cobra.Command {
+	var short, commit bool
 	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "report the version of linuxkit",
-		Long:  `Report the version of linuxkit.`,
+		Long: `Report the version of linuxkit.
+		Run with option --short to print just the version number.
+		Run with option --commit to print just the git commit.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if short {
+				fmt.Println(version.Version)
+				return nil
+			}
+			if commit {
+				fmt.Println(version.GitCommit)
+				return nil
+			}
 			fmt.Printf("%s version %s\n", filepath.Base(os.Args[0]), version.Version)
 			if version.GitCommit != "" {
 				fmt.Printf("commit: %s\n", version.GitCommit)
@@ -22,6 +33,8 @@ func versionCmd() *cobra.Command {
 			return nil
 		},
 	}
+	cmd.Flags().BoolVar(&short, "short", false, "print just the version number")
+	cmd.Flags().BoolVar(&commit, "commit", false, "print just the commit")
 
 	return cmd
 }
