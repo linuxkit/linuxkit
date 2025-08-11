1caf2feffc94ca4edb14dbb3c0002343546e990d
Avi Deitcher <deitch@users.noreply.github.com>
2025-08-11 11:58:17 +0300

    add support for custom build args (#4155)
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

---
+++ src/cmd/linuxkit/pkglib/const.go
@@ -0,0 +1,8 @@
+package pkglib
+
+const (
+	// DefaultPkgBuildYML is the default name of the package build file
+	DefaultPkgBuildYML = "build.yml"
+	// DefaultPkgCommit is the default commit to use for packages
+	DefaultPkgCommit = "HEAD"
+)
