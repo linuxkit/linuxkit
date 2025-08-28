c3228fb526b97a421b48b5b2d5a2b7004e4c7e6f
Avi Deitcher <deitch@users.noreply.github.com>
2025-08-28 10:34:53 +0300

    erofs output (#4162)
    * add erofs as output option
    
    Signed-off-by: Avi Deitcher <avi@deitcher.net>
    
    * unify nearly identical functions
    
    Signed-off-by: Avi Deitcher <avi@deitcher.net>
    
    ---------
    
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

---
+++ test/cases/000_build/000_formats/014_kernel+erofs/test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# SUMMARY: Check that kernel+squashfs output format is generated
+# LABELS:
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+NAME=check
+
+clean_up() {
+	rm -f ${NAME}*
+}
+
+trap clean_up EXIT
+
+linuxkit build --format kernel+erofs --name "${NAME}" ../test.yml
+[ -f "${NAME}-kernel" ] || exit 1
+[ -f "${NAME}-erofs.img" ] || exit 1
+[ -f "${NAME}-cmdline" ] || exit 1
+
+exit 0
