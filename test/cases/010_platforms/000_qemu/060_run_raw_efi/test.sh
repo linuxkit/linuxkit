56e37a842ef4e6b6420a0d776414e5cfc385be1e
Chris Irrgang <chris.irrgang@gmx.de>
2025-11-03 23:36:40 +0000

    Add raw-efi test + documentation
    Signed-off-by: Chris Irrgang <chris.irrgang@gmx.de>

---
+++ test/cases/010_platforms/000_qemu/060_run_raw_efi/test.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# SUMMARY: Check that EFI BIOS ISO boots in qemu
+# LABELS:
+
+set -e
+
+# Source libraries. Uncomment if needed/defined
+#. "${RT_LIB}"
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+NAME=qemu-raw-efi
+
+clean_up() {
+	rm -rf ${NAME}-*
+}
+trap clean_up EXIT
+
+# see https://github.com/linuxkit/linuxkit/issues/1872 this is very flaky in qemu
+# disabling for now until we fix a config that works
+exit $RT_CANCEL
+
+if command -v qemu-system-x86_64; then
+	if [ ! -f /usr/share/ovmf/bios.bin ]; then
+		exit $RT_CANCEL
+	fi
+fi
+
+linuxkit build --format raw-efi --name "${NAME}" test.yml
+[ -f "${NAME}-efi.img" ] || exit 1
+linuxkit run qemu --uefi "${NAME}-efi.img" | grep -q "Welcome to LinuxKit"
+
+exit 0
