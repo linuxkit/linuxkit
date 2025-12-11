a066b29c1db1d65941b19fed63d7d64c27fadd3f
Chris Irrgang <chris.irrgang@gmx.de>
2025-12-11 00:52:30 +0000

    fix remaining two files for 6.6.71 kernel hash update
    Signed-off-by: Chris Irrgang <chris.irrgang@gmx.de>

--- test/cases/020_kernel/219_tags_6.6.x/test.sh
+++ test/cases/020_kernel/219_tags_6.6.x/test.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-KERNEL=linuxkit/kernel:6.6.71-01ab8e22c88f25fc1bf4c354689f12797c213a86
+KERNEL=linuxkit/kernel:6.6.71-fac3a2ad495a23a50c8d2d7f173ecc7145049c29
 
 # just include the common test
 . ../tags.sh
